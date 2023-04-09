7b16953f4f71e4f467bc69bad9f1084a59a346b5
Abdul7lah <111016023+Abdul7lah@users.noreply.github.com>
2023-04-09 02:26:03 -0400

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './Components/Footer';
 import Home from './Components/Home';
 
 function App() {
   return (
     <div>
-  <Home></Home>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
